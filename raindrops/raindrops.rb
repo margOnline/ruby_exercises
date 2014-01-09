f1cb22bf8a5d9a54949df91b8549edd4a2ab2670
Margo <margo@margonline.co.uk>
2014-01-09 19:14:29 +0000

    better method names

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,22 +1,22 @@
 class Raindrops
 
   def convert num
-    unless prime_factor?(num)
+    unless has_prime_factor?(num)
       return num.to_s
     end
 
     result = ''
-    result << 'Pling' if div_by?(3, num)
-    result << 'Plang' if div_by?(5, num)
-    result << 'Plong' if div_by?(7, num)
+    result << 'Pling' if divisible_by?(3, num)
+    result << 'Plang' if divisible_by?(5, num)
+    result << 'Plong' if divisible_by?(7, num)
     result
   end
 
-  def prime_factor?(num)
-    (div_by?(3, num) || div_by?(5, num) || div_by?(7 ,num)) 
+  def has_prime_factor?(num)
+    (divisible_by?(3, num) || divisible_by?(5, num) || divisible_by?(7 ,num)) 
   end
 
-  def div_by?(divisor, num)
+  def divisible_by?(divisor, num)
     num % divisor == 0    
   end
 
